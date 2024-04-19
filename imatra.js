86d6652ed660e3bf61f89b6d1c795a3702f7a038
Juha Tauriainen <juha@bin.fi>
2024-04-19 22:28:06 +0300

    Refactor code

--- imatra.js
+++ imatra.js
@@ -2,18 +2,51 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const sushi = require("./points");
+const { sushi1, sushi2 } = require("./points");
 
 app.use(express.json());
 
 const sum = (points) => points.reduce((a, b) => a + b, 0);
 
-sushi.forEach((sushi) => {
-  console.log(sushi.nimi, sum(sushi.points), sushi.points);
+const sortFn = (a, b) =>
+  sum(b.points) + b.pudding - (sum(a.points) + a.pudding);
+
+const firstRound = [...sushi1].sort(sortFn);
+const secondRound = [...sushi2].sort(sortFn);
+
+const rounds = [firstRound, secondRound];
+
+rounds.forEach((round) => {
+  round.forEach((sushi) => {
+    console.log(
+      sushi.nimi,
+      sum(sushi.points) + sushi.pudding,
+      sushi.points,
+      "pudding: ",
+      sushi.pudding
+    );
+  });
+  console.log("-----");
 });
 
+const createRound = (round) =>
+  round.map((result) => ({
+    nimi: result.nimi,
+    points: result.points,
+    pudding: result.pudding,
+    total: sum(result.points) + result.pudding,
+  }));
+
 app.get("/", (req, res) => {
-  res.json(sushi);
+  const data = rounds.reduce(
+    (a, b, index) => ({
+      [`game${index + 1}`]: createRound(b),
+      ...a,
+    }),
+    {}
+  );
+
+  res.json(data);
 });
 
 app.listen(port, () => {
--- points.js
+++ points.js
@@ -1,33 +1,39 @@
 const sushi1 = [
   {
     nimi: "juhq",
-    points: [3 + 5 + 6, 3 + 1 + 2 + 5 + 5, 2 + 4 + 1 + 6],
+    points: [3 + 5 + 6, 3 + 1 + 2 + 5 + 5, 2 + 4 + 1],
+    pudding: 6,
   },
 
   {
     nimi: "tero",
-    points: [3 + 10 + 10, 1 + 6 + 10 + 5 + 2, -3 + 3 + 6 + 3],
+    points: [3 + 10 + 10, 1 + 6 + 10 + 5 + 2, 3 + 6 + 3],
+    pudding: -3,
   },
   {
     nimi: "oliver",
-    points: [6 + 1, 1 + 16, -3 + 1 + 5 + 6 + 1],
+    points: [1 + 6 + 1, 1 + 16, 1 + 5 + 6 + 1],
+    pudding: -3,
   },
 ];
 
-const sushi = [
+const sushi2 = [
   {
     nimi: "juhq",
-    points: [3 + 5 + 6 + 1, 3 + 1 + 6, 2 + 6 + 3 + 6],
+    points: [3 + 5 + 6 + 1, 3 + 1 + 6, 2 + 6 + 3],
+    pudding: 6,
   },
 
   {
     nimi: "tero",
-    points: [6 + 6, 5 + 3 + 6 + 1, -3 + 6 + 1 + 3],
+    points: [6 + 6, 5 + 3 + 6 + 1, 6 + 1 + 3],
+    pudding: -3,
   },
   {
     nimi: "oliver",
-    points: [3 + 2 + 10 + 5, 3 + 6, 1 + 1 + 5 + -3],
+    points: [3 + 2 + 10 + 5, 3 + 6, 1 + 1 + 5],
+    pudding: -3,
   },
 ];
 
-module.exports = sushi;
+module.exports = { sushi1, sushi2 };
